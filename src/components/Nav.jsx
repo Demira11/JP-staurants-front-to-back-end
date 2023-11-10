3e97f51b4b39768f71c156f44315b24851172a48
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-11-10 14:02:13 -0500

    tying to change the nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav>
+    <nav className="nav-vontainer">
       <NavLink to="/">Home</NavLink> <br />
       <NavLink to="/restaurants">Find Restaurants</NavLink> <br />
       <NavLink to="/add-restaurant">Add New Restaurant!</NavLink>
