6be81304f1c78b1cd7abaf9aed02c32833e07c6b
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-11-10 13:03:58 -0500

    added a restaurants.js file into the services folder

---
+++ src/services/restaurants.js
@@ -0,0 +1,46 @@
+import api from "./apiConfig.js";
+
+export const getRestaurants = async () => {
+  try {
+    const response = await api.get("/restaurants");
+    return response.data;
+  } catch (error) {
+    console.error("Error: Getting all Restaurants: ", error);
+  }
+};
+
+export const getRestaurant = async (id) => {
+  try {
+    const response = await api.get(`/restaurants/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error: Getting one Restaurant: ", error);
+  }
+};
+
+export const createRestaurant = async (catData) => {
+  try {
+    const response = await api.post("/restaurants", restaurantData);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const editRestaurant = async (id, catData) => {
+  try {
+    const response = await api.put(`/restaurants/${id}`, restaurantData);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const deleteRestaurant = async (id) => {
+  try {
+    const response = await api.delete(`/restaurants/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
