5ddf18172a9174e2e4220de4ba944bb94e35bc1b
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-11-11 16:03:37 -0500

    played with the home.jsx. because my functionality doesn't wotk

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,55 +1,17 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 
 function Home() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const navigate = useNavigate();
-
-  const handleSearch = async () => {
-    try {
-      const response = await fetch(
-        `https://restaurant-api1-0cadf6d31e8f.herokuapp.com/api/restaurants?city=${searchTerm}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            // Add any other headers you may need (e.g., authentication headers)
-          },
-        }
-      );
-
-      if (!response.ok) {
-        console.error("Error searching for restaurants:", response.statusText);
-        return;
-      }
-
-      const searchResults = await response.json();
-      console.log("Search results:", searchResults);
-
-      // You can navigate to a search results page or update state to render the results
-      // For now, let's log the results and clear the search term
-      setSearchTerm("");
-    } catch (error) {
-      console.error("Error during search:", error.message);
-    }
-  };
-
   return (
     <div>
-      <h1 className="home">Find your restaurant here!</h1>
+      <h1 className="home">Anywhere in New York, find your restaurant here!</h1>
       <div className="search-container">
-        <input
-          className="search-box"
-          type="text"
-          placeholder="Search Restaurant by city..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button className="search-button-home" onClick={handleSearch}>
-          Go
-        </button>
+        <input type="text" class="search-box" placeholder="Enter City..." />
+        <button class="search-button-home">Go</button>
       </div>
-      {/* Rest of your content */}
+      <img
+        src="https://dineatdome.com/wp-content/uploads/2022/10/308909473_534328565361801_4905719160182160214_n.jpeg"
+        alt="colorful food"
+      />
     </div>
   );
 }
