400f04b986e55d7237f2da651caa63d5568fcf74
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-11-11 15:43:21 -0500

    added some logic to home.jsx file

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(
+        `https://restaurant-api1-0cadf6d31e8f.herokuapp.com/api/restaurants?city=${searchTerm}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            // Add any other headers you may need (e.g., authentication headers)
+          },
+        }
+      );
+
+      if (!response.ok) {
+        console.error("Error searching for restaurants:", response.statusText);
+        return;
+      }
+
+      const searchResults = await response.json();
+      console.log("Search results:", searchResults);
+
+      // You can navigate to a search results page or update state to render the results
+      // For now, let's log the results and clear the search term
+      setSearchTerm("");
+    } catch (error) {
+      console.error("Error during search:", error.message);
+    }
+  };
+
   return (
     <div>
-      <h1 className="home">Find you restaurant here!</h1>
-      <div class="search-container">
+      <h1 className="home">Find your restaurant here!</h1>
+      <div className="search-container">
         <input
-          class="search-box"
+          className="search-box"
           type="text"
           placeholder="Search Restaurant by city..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <button class="search-button-home">Go</button>
+        <button className="search-button-home" onClick={handleSearch}>
+          Go
+        </button>
       </div>
-      <img
-        src="https://dineatdome.com/wp-content/uploads/2022/10/308909473_534328565361801_4905719160182160214_n.jpeg"
-        alt="colorful food"
-        class="responsive"
-      />
+      {/* Rest of your content */}
     </div>
   );
 }
