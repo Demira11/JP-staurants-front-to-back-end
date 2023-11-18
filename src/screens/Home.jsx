f3673791b3f17e53a69a573b8e0f83b4a58039f3
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-11-18 17:54:52 -0500

    Added event listener to the search button

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -8,6 +8,14 @@ function Home() {
   const handleSearch = () => {
     navigate(`/restaurants?city=${encodeURIComponent(city)}`);
   };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleSearch();
+    }
+  };
+
   const capitalizeFirstLetter = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
   };
@@ -22,6 +30,7 @@ function Home() {
           placeholder="Enter City..."
           value={city}
           onChange={(e) => setCity(capitalizeFirstLetter(e.target.value))}
+          onKeyPress={handleKeyPress}
         />
         <button className="search-button-home" onClick={handleSearch}>
           Go
@@ -36,3 +45,42 @@ function Home() {
 }
 
 export default Home;
+
+// import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+
+// function Home() {
+//   const [city, setCity] = useState("");
+//   const navigate = useNavigate();
+
+//   const handleSearch = () => {
+//     navigate(`/restaurants?city=${encodeURIComponent(city)}`);
+//   };
+//   const capitalizeFirstLetter = (str) => {
+//     return str.charAt(0).toUpperCase() + str.slice(1);
+//   };
+
+//   return (
+//     <div>
+//       <h1 className="home">Anywhere in New York, find your restaurant here!</h1>
+//       <div className="search-container">
+//         <input
+//           type="text"
+//           className="search-box"
+//           placeholder="Enter City..."
+//           value={city}
+//           onChange={(e) => setCity(capitalizeFirstLetter(e.target.value))}
+//         />
+//         <button className="search-button-home" onClick={handleSearch}>
+//           Go
+//         </button>
+//       </div>
+//       <img
+//         src="https://www.tastingtable.com/img/gallery/best-italian-restaurants-in-nyc/l-intro-1687816141.jpg"
+//         alt="colorful food"
+//       />
+//     </div>
+//   );
+// }
+
+// export default Home;
