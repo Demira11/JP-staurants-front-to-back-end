321dd105c3fe88dd0567898df5fc15dbff058ed1
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-11-18 17:23:38 -0500

    added background color to restaurant page after searching and a grid layout

--- src/screens/Restaurants.jsx
+++ src/screens/Restaurants.jsx
@@ -21,13 +21,11 @@ function Restaurants() {
       (restaurant) => restaurant.city === city
     );
 
-    console.log(allRestaurants);
-    console.log(filteredRestaurants);
     setRestaurants(filteredRestaurants);
   }
 
   return (
-    <div>
+    <div className="restaurants-page">
       <h1>All the Restaurants in {city}!</h1>
       <div className="restaurants-container">
         {restaurants?.map((restaurant) => (
@@ -37,4 +35,46 @@ function Restaurants() {
     </div>
   );
 }
+
 export default Restaurants;
+
+// import React, { useState, useEffect } from "react";
+// import { useLocation } from "react-router-dom";
+// import { getRestaurants } from "../services/restaurants.js";
+// import Restaurant from "../components/Restaurant.jsx";
+
+// function Restaurants() {
+//   const [restaurants, setRestaurants] = useState([]);
+//   const location = useLocation();
+//   const city = new URLSearchParams(location.search).get("city");
+
+//   useEffect(() => {
+//     if (city) {
+//       fetchRestaurants();
+//     }
+//   }, [city]);
+
+//   async function fetchRestaurants() {
+//     const allRestaurants = await getRestaurants();
+
+//     const filteredRestaurants = allRestaurants?.filter(
+//       (restaurant) => restaurant.city === city
+//     );
+
+//     console.log(allRestaurants);
+//     console.log(filteredRestaurants);
+//     setRestaurants(filteredRestaurants);
+//   }
+
+//   return (
+//     <div>
+//       <h1>All the Restaurants in {city}!</h1>
+//       <div className="restaurants-container">
+//         {restaurants?.map((restaurant) => (
+//           <Restaurant key={restaurant._id} restaurant={restaurant} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+// export default Restaurants;
